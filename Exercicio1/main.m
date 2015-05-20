df87fe4abcf42a02e53d46beff91db63275b7e5f
Daniele Cebin <daniele.cebin@movile.com>
2015-05-20 20:29:27 -0300

    Initial Commit

---
+++ Exercicio1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Exercicio1
+//
+//  Created by Daniele Cebin on 20/05/15.
+//  Copyright (c) 2015 Daniele Cebin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
